238b5df63e95686b43c01c516b09432314ce0681
Onowuckc <ijomahchinazaaugustine@gmail.com>
2025-12-09 20:05:41 +0100

    simulation fix 2

--- src/controllers/simulationController.js
+++ src/controllers/simulationController.js
@@ -68,8 +68,7 @@ const simulateFundAccount = async (req, res) => {
       headers: {
         'Content-Type': 'application/json;schema=PAAS',
         'x-reap-api-key': apiKey,
-        'x-reap-entity-id': entityId,
-        'Accept': 'application/vnd.api+json; version=1.0.0'
+        'x-reap-entity-id': entityId
       },
       body: JSON.stringify(payload)
     });
